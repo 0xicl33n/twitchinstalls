57174cbb12591551f370d4d0374047d5c38a0e8f
chris forst <0xicl33n@gmail.com>
2015-10-31 10:28:10 -0700

    run as root

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-su
+
+FILE="/tmp/out.$$"
+GREP="/bin/grep"
+# Make sure only root can run this
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+      exit 1
+      fi
+
 pacman -Syuu
 pacman -S --noconfirm base-devel zsh screen nmap openssh i3-wm xorg-core vba python2-minimal irssi i3status dmenu
 
